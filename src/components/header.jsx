bbe308f62e742515fdc62649a6b16d007da51e80
Guilherme Diel <guiddiel@gmail.com>
2024-01-22 08:23:05 -0300

    first page

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <div className="container-fluid head">
+            <div className="row row-cols-2">
+                <div className="col">
+                    OIE
+                </div>
+                <div className="col">
+                    OIE2
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,18 @@
+import Header from '../components/header';
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <h1>Home</h1>
+            <div className="Container">
+                <div className="Row">
+                    <div className="col">
+                        <div>
+                            loren ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
